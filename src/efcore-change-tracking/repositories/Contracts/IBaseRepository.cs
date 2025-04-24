5b7cb6e260753c8d785b19bafb453b2f898065da
Ashleigh Clark <ashleigh.clark@kaleidocode.co.za>
2025-04-24 10:07:55 +0200

    ADD: Signatures to Interface

--- src/efcore-change-tracking/repositories/Contracts/IBaseRepository.cs
+++ src/efcore-change-tracking/repositories/Contracts/IBaseRepository.cs
@@ -11,8 +11,26 @@ namespace Kaleidocode.Gists.ChangeTracking.Repositories.Contracts
     {
         void Add(TEntity entity);
 
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        void AddRange(IEnumerable<TEntity> entities);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
         void Update(TEntity entity);
 
-        void Delete(TEntity entity);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+        void Remove(TEntity entity);
+
+        Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
     }
 }
